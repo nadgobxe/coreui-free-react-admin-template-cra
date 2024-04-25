02e268a301092e6377e9d49cdbd623345c00b6ee
nadgobxe <bogdan.burcea22@my.northampton.ac.uk>
2024-04-25 14:46:57 +0100

    Update rejectUnauthorized option and secureProtocol in serverProxy.js

--- netlify/functions/serverProxy.js
+++ netlify/functions/serverProxy.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
-// const https = require('https')
+const https = require('https')
 
-// const agent = new https.Agent({
-//   rejectUnauthorized: true, // Set to true for production
-//   secureProtocol: 'TLSv1_2_method' // Force TLS 1.2 as the protocol
-// });
+const agent = new https.Agent({
+  rejectUnauthorized: true, // Set to true for production
+  secureProtocol: 'TLSv1_2_method' // Force TLS 1.2 as the protocol
+});
 
 exports.handler = async (event, context) => {
   const { path, httpMethod, headers, body } = event
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
       url: `${backendUrl}${cleanedUrl}`,
       headers,
       data: body,
-      // httpsAgent: agent, // Add the httpsAgent option
+      httpsAgent: agent, // Add the httpsAgent option
     })
     console.log('Received event:', event)
     console.log('Making request to backend:', `${backendUrl}${cleanedUrl}`)
