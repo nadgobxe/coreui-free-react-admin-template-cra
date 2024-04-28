7df45fce48bb6068201ea3c5e06bd0873d74c9fe
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2024-04-28 22:07:06 +0100

    Update console logs and error handling in serverProxy.js

--- netlify/functions/serverProxy.js
+++ netlify/functions/serverProxy.js
@@ -1,21 +1,28 @@
-const axios = require('axios');
-const https = require('https');
+const axios = require('axios')
+const https = require('https')
 
 const agent = new https.Agent({
   rejectUnauthorized: false, // Set to true for production
-});
+})
 
 exports.handler = async (event, context) => {
-  const { path, httpMethod, headers, body } = event;
-  const backendUrl = 'https://holdemserver-1dfb99f436f4.herokuapp.com'; // Replace with your backend server URL
+  const { path, httpMethod, headers, body } = event
+  const backendUrl = 'https://holdemserver-1dfb99f436f4.herokuapp.com' // Replace with your backend server URL
 
-  console.log('Received event:', event);
+  console.log('Received event:', event)
+  console.log('Path:', path)
+  console.log('HTTP method:', httpMethod)
+  console.log('Headers:', headers)
+  console.log('Body:', body)
+  console.log('Backend URL:', backendUrl)
+  console.log('Agent:', agent)
+  console.log('Context', context)
 
   // Remove the Netlify Function path from the URL
-  const cleanedPath = path.replace('/.netlify/functions/serverProxy', '');
+  const cleanedPath = path.replace('/.netlify/functions/serverProxy', '')
 
-  const backendEndpoint = `${backendUrl}${cleanedPath}`;
-  console.log('Making request to backend:', backendEndpoint);
+  const backendEndpoint = `${backendUrl}${cleanedPath}`
+  console.log('Making request to backend:', backendEndpoint)
 
   try {
     const response = await axios({
@@ -24,25 +31,25 @@ exports.handler = async (event, context) => {
       headers,
       data: body,
       httpsAgent: agent,
-    });
+    })
 
-    console.log('Backend response status:', response.status);
-    console.log('Backend response data:', response.data);
+    console.log('Backend response status:', response.status)
+    console.log('Backend response data:', response.data)
 
     return {
       statusCode: response.status,
       headers: response.headers,
       body: JSON.stringify(response.data),
-    };
+    }
   } catch (error) {
-    console.error('Error response status:', error.response?.status);
-    console.error('Error occurred while proxying the request:', error);
-    console.error('Error response status:', error.response?.status);
-    console.error('Error response data:', error.response?.data);
+    console.error('Error response status:', error.response?.status)
+    console.error('Error occurred while proxying the request:', error)
+    console.error('Error response status:', error.response?.status)
+    console.error('Error response data:', error.response?.data)
 
     return {
       statusCode: error.response?.status || 500,
       body: JSON.stringify({ error: 'An error occurred while proxying the request.' }),
-    };
+    }
   }
-};
+}
