48132a772f0e136e66415fd388ff66299a649951
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2024-04-28 21:10:57 +0100

    Update rejectUnauthorized option and secureProtocol in serverProxy.js

--- netlify/functions/serverProxy.js
+++ netlify/functions/serverProxy.js
@@ -1,42 +1,43 @@
-const axios = require('axios')
-const https = require('https')
+const axios = require('axios');
+const https = require('https');
 
 const agent = new https.Agent({
   rejectUnauthorized: false, // Set to true for production
 });
 
 exports.handler = async (event, context) => {
-  const { path, httpMethod, headers, body } = event
-  const backendUrl = 'https://holdemserver-1dfb99f436f4.herokuapp.com' // Replace with your backend server URL
-  console.log('Received event:', event)
-  console.log('Making request to backend:', `${backendUrl}${path}`)
+  const { path, httpMethod, headers, body } = event;
+  const backendUrl = 'https://holdemserver-1dfb99f436f4.herokuapp.com'; // Replace with your backend server URL
 
-  // Define the part of the URL to remove
-  const partToRemove = '/.netlify/functions/serverProxy/'
+  console.log('Received event:', event);
 
-  // Remove the specified part
-  const cleanedUrl = path.replace(partToRemove, '/')
+  // Remove the Netlify Function path from the URL
+  const cleanedPath = path.replace('/.netlify/functions/serverProxy', '');
+
+  const backendEndpoint = `${backendUrl}${cleanedPath}`;
+  console.log('Making request to backend:', backendEndpoint);
 
   try {
     const response = await axios({
       method: httpMethod,
-      url: `${backendUrl}${cleanedUrl}`,
+      url: backendEndpoint,
       headers,
       data: body,
-      httpsAgent: agent, // Add the httpsAgent option
-    })
-    console.log('Received event:', event)
-    console.log('Making request to backend:', `${backendUrl}${cleanedUrl}`)
+      httpsAgent: agent,
+    });
+
+    console.log('Backend response:', response.data);
+
     return {
       statusCode: response.status,
       headers: response.headers,
       body: JSON.stringify(response.data),
-    }
+    };
   } catch (error) {
-    console.error('Error occurred while proxying the request:', error)
+    console.error('Error occurred while proxying the request:', error);
     return {
       statusCode: error.response?.status || 500,
       body: JSON.stringify({ error: 'An error occurred while proxying the request.' }),
-    }
+    };
   }
-}
+};
