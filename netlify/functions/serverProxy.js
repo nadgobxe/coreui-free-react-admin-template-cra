6542d5318f167b0170d36a0fc39b96db1a609824
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2024-04-28 22:16:39 +0100

    Refactor console logs and error handling in serverProxy.js

--- netlify/functions/serverProxy.js
+++ netlify/functions/serverProxy.js
@@ -57,19 +57,22 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: error.response?.status || 500,
-      console.log('Received event:', event)
-      console.log('Path:', path)
-      console.log('HTTP method:', httpMethod)
-      console.log('Headers:', headers)
-      console.log('Body:', body)
-      console.log('Backend URL:', backendUrl)
-      console.log('Agent:', agent)
-      console.log('Context', context)
-      console.error('Error response status:', error.response?.status)
-      console.error('Error occurred while proxying the request:', error)
-      console.error('Error response status:', error.response?.status)
-      console.error('Error response data:', error.response?.data)
-      body: JSON.stringify({ error: 'An error occurred while proxying the request.' }),
+      body: JSON.stringify({
+        error: 'An error occurred while proxying the request.',
+        logs: {
+          event,
+          path,
+          httpMethod,
+          headers,
+          body,
+          backendUrl,
+          agent,
+          context,
+          errorResponseStatus: error.response?.status,
+          errorResponseData: error.response?.data,
+          errorMessage: error.message,
+        },
+      }),
     }
   }
 }
