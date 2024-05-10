fc24ea212efc7a41e35cbaaff5a3d68727b167d8
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2024-05-10 21:52:15 +0100

    Update EmployeeDashboard.js to include user picture in the card image

--- src/views/dashboard/EmployeeDashboard.js
+++ src/views/dashboard/EmployeeDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { CButton, CCard, CCardBody } from '@coreui/react'
+import { CButton, CCard, CCardBody, CCardImage, CCardTitle, CCardText } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilUser } from '@coreui/icons'
 import { useSelector } from 'react-redux'
@@ -12,7 +12,7 @@ const EmployeeDashboard = () => {
   return (
     <>
       <CCard style={{ width: '18rem' }}>
-        <CCardImage orientation="top" src={ReactImg} />
+        <CCardImage orientation="top" src={user.user.picture} />
         <CCardBody>
           <CCardTitle>Card title</CCardTitle>
           <CCardText>
