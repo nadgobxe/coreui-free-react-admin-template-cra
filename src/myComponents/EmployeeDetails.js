001f628ef9aa7a6daacc2989d4bc7d412e24b20c
nadgobxe <bogdan.burcea22@my.northampton.ac.uk>
2024-05-31 12:23:17 +0100

    refactor: Update EmployeeDetails.js to fix column styling issue

--- src/myComponents/EmployeeDetails.js
+++ src/myComponents/EmployeeDetails.js
@@ -119,7 +119,7 @@ const EmployeeDetails = () => {
   }
 
   const columns = [
-    { key: 'id', label: '#', _props: { scope: 'col' }, className: 'text-red d-none d-md-table-cell' },
+    { key: 'id', label: '#', _props: { className: 'text-red d-none d-md-table-cell', scope: 'col' } },
     { key: 'date', label: 'Date', _props: { scope: 'col' } },
     { key: 'colleagues', label: 'Colleagues', _props: { scope: 'col' } },
     { key: 'postcodes', label: 'Postcodes', _props: { scope: 'col' } },
